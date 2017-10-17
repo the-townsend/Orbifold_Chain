62fef4adbfd05c4757842fdbeac649baa480ed6a
the-townsend <cft71@hotmail.com>
2017-10-17 20:16:39 +0100

    Removing hi there

--- Orbifold_Chain/Orbifold_Chain/Function-Token.cs
+++ Orbifold_Chain/Orbifold_Chain/Function-Token.cs
@@ -6,11 +6,6 @@ using System.Threading.Tasks;
 
 namespace Orbifold_Chain
 {
-
-    //HI there!
-
-
-
     /// <summary>
     /// Function Token consists of an amount(x), a time point(t), a Token, a value function and a time lag function. 
     /// Token pays out x times function(t) at point in time timelag(t). 
